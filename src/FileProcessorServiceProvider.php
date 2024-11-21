9d2a36188d42e5dc4f070014307ac20f208e7e4f
DevPhplaravel <agendabet5@gmail.com>
2024-11-21 12:08:13 +0100

    modifica commenti

--- src/FileProcessorServiceProvider.php
+++ src/FileProcessorServiceProvider.php
@@ -8,7 +8,7 @@ class FileProcessorServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Registra eventuali binding o configurazioni
+        // Registro eventuali binding o configurazioni
         $this->mergeConfigFrom(__DIR__ . '/../src/config/file-processor.php', 'file-processor');
     }
 
@@ -20,23 +20,23 @@ class FileProcessorServiceProvider extends ServiceProvider
         \DevPHPLaravel\ImportFileProcessor\Command\ImportFinancialData::class, // Comando nel pacchetto
     ]);
 
-        // Pubblica il file di configurazione
+        // Pubblico il file di configurazione
     $this->publishes([
         __DIR__ . '/../src/config/file-processor.php' => config_path('file-processor.php'),
     ], 'config');
 
 
-        // Carica le migrazioni
+        // Carico le migrazioni
         $this->loadMigrationsFrom(__DIR__.'/../migrations');
 
 
-        // Crea la directory import-files se non esiste
+        // Creo la directory import-files se non esiste
         $this->createImportFilesDirectory();
     }
 
     protected function createImportFilesDirectory()
     {
-        // Usa il disco 'local' per creare la directory
+        // Uso il disco 'local' per creare la directory
         $path = 'import-files';
 
         // Verifica se la directory esiste, altrimenti la crea
